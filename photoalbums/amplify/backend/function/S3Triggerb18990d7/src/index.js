1116a27a81a2642359e6bc9276c310a7e08cfd17
Gabe Hollombe <gabe@avantbard.com>
2020-02-06 15:11:05 +0800

    amplify function update to add api privs to s3trigger

--- photoalbums/amplify/backend/function/S3Triggerb18990d7/src/index.js
+++ photoalbums/amplify/backend/function/S3Triggerb18990d7/src/index.js
@@ -1,4 +1,11 @@
-// eslint-disable-next-line
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var apiPhotoalbumsGraphQLAPIIdOutput = process.env.API_PHOTOALBUMS_GRAPHQLAPIIDOUTPUT
+var apiPhotoalbumsGraphQLAPIEndpointOutput = process.env.API_PHOTOALBUMS_GRAPHQLAPIENDPOINTOUTPUT
+
+Amplify Params - DO NOT EDIT */// eslint-disable-next-line
 exports.handler = function(event, context) {
   console.log('Received S3 event:', JSON.stringify(event, null, 2));
   // Get the object from the event and show its content type
