afdc9f598ebf84023a317f7cc81a5933f566e39e
Gabe Hollombe <gabe@avantbard.com>
2020-02-06 14:58:34 +0800

    update frontend to allow for uploading and rendering of photos

--- photoalbums/src/App.js
+++ photoalbums/src/App.js
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react';
 
 import Amplify, {Auth} from 'aws-amplify'
 import API, {graphqlOperation} from '@aws-amplify/api'
+import Storage from '@aws-amplify/storage'
 import aws_exports from './aws-exports'
 
-import {withAuthenticator} from 'aws-amplify-react'
-import {Grid, Header, Input, List, Segment} from 'semantic-ui-react'
+import {S3Image, withAuthenticator} from 'aws-amplify-react'
+import {Divider, Form, Grid, Header, Input, List, Segment} from 'semantic-ui-react'
 
 import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+
+import {v4 as uuid} from 'uuid';
 
 import * as queries from './graphql/queries'
 import * as mutations from './graphql/mutations'
@@ -117,8 +120,11 @@ const AlbumsList = () => {
 }
 
 const AlbumDetails = (props) => {
-  const [album,
-    setAlbum] = useState({name: 'Loading...', photos: []})
+  const [album, setAlbum] = useState({name: 'Loading...', photos: []})
+  const [photos, setPhotos] = useState([])
+  const [hasMorePhotos, setHasMorePhotos] = useState(true)
+  const [fetchingPhotos, setFetchingPhotos] = useState(false)
+  const [nextPhotosToken, setNextPhotosToken] = useState(null)
 
   useEffect(() => {
     const loadAlbumInfo = async() => {
@@ -129,14 +135,137 @@ const AlbumDetails = (props) => {
     loadAlbumInfo()
   }, [props.id])
 
+  useEffect(() => {
+    fetchNextPhotos()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    let subscription
+    async function setupSubscription() {
+      const user = await Auth.currentAuthenticatedUser()
+      subscription = API.graphql(graphqlOperation(subscriptions.onCreatePhoto, {owner: user.username})).subscribe({
+        next: (data) => {
+          const photo = data.value.data.onCreatePhoto
+          if (photo.albumId !== props.id) return
+            setPhotos(p => p.concat([photo]))
+        }
+      })
+    }
+    setupSubscription()
+
+    return () => subscription.unsubscribe();
+  }, [props.id])
+
+
+  const fetchNextPhotos = async () => {
+    const FETCH_LIMIT = 20
+    setFetchingPhotos(true)
+    let queryArgs = {
+      albumId: props.id,
+      limit: FETCH_LIMIT, 
+      nextToken: nextPhotosToken
+    }
+    if (! queryArgs.nextToken) delete queryArgs.nextToken
+    const results = await API.graphql(graphqlOperation(queries.listPhotosByAlbum, queryArgs))
+    setPhotos(p => p.concat(results.data.listPhotosByAlbum.items))
+    setNextPhotosToken(results.data.listPhotosByAlbum.nextToken)
+    setHasMorePhotos(results.data.listPhotosByAlbum.items.length === FETCH_LIMIT)
+    setFetchingPhotos(false)
+  }
+
   return (
     <Segment>
       <Header as='h3'>{album.name}</Header>
-      <p>TODO LATER IN WORKSHOP: Allow photo uploads</p>
-      <p>TODO LATER IN WORKSHOP: Show photos for this album</p>
+      <S3ImageUpload albumId={album.id} />
+      <PhotosList photos={photos} />
+      {
+          hasMorePhotos && 
+          <Form.Button
+            onClick={() => fetchNextPhotos()}
+            icon='refresh'
+            disabled={fetchingPhotos}
+            content={fetchingPhotos ? 'Loading...' : 'Load more photos'}
+          />
+      }
     </Segment>
   )
 }
+
+
+const S3ImageUpload = (props) => {
+  const [uploading, setUploading] = useState(false)
+  
+  const uploadFile = async (file) => {
+    const fileName = 'upload/'+uuid();
+    const user = await Auth.currentAuthenticatedUser();
+
+    const result = await Storage.vault.put(
+      fileName, 
+      file, 
+      {
+        metadata: {
+          albumid: props.albumId,
+          owner: user.username,
+        }
+      }
+    );
+
+    console.log('Uploaded file: ', result);
+  }
+
+  const onChange = async (e) => {
+    setUploading(true)
+    
+    let files = [];
+    for (var i=0; i<e.target.files.length; i++) {
+      files.push(e.target.files.item(i));
+    }
+    await Promise.all(files.map(f => uploadFile(f)));
+
+    setUploading(false)
+  }
+
+  return (
+    <div>
+      <Form.Button
+        onClick={() => document.getElementById('add-image-file-input').click()}
+        disabled={uploading}
+        icon='file image outline'
+        content={ uploading ? 'Uploading...' : 'Add Images' }
+      />
+      <input
+        id='add-image-file-input'
+        type="file"
+        accept='image/*'
+        multiple
+        onChange={onChange}
+        style={{ display: 'none' }}
+      />
+    </div>
+  );
+}
+
+const PhotosList = React.memo((props) => {
+  const PhotoItems = (props) => {
+    return props.photos.map(photo =>
+      <S3Image 
+        key={photo.thumbnail.key} 
+        imgKey={'resized/' + photo.thumbnail.key.replace(/.+resized\//, '')}
+        level="private"
+        style={{display: 'inline-block', 'paddingRight': '5px'}}
+      />
+    );
+  }
+
+  return (
+    <div>
+      <Divider hidden />
+      <PhotoItems photos={props.photos} />
+    </div>
+  );
+})
+
 
 function App() {
   return (
