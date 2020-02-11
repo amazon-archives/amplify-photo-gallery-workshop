daa4069beb70d721535869f7078c9ad1c27b03c0
Gabe Hollombe <gabehol@amazon.com>
2020-02-11 13:53:51 +0800

    integrate auth to frontend

--- photoalbums/src/App.js
+++ photoalbums/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+import { withAuthenticator } from 'aws-amplify-react';
 import { Header } from 'semantic-ui-react';
+
+Amplify.configure(aws_exports);
 
 function App() {
   return (
     <Header as="h1">
       Hello World!
     </Header>
-  )
+  );
 }
 
-export default App
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig: {
+    hiddenDefaults: ['phone_number']
+  }
+});
